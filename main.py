7c9df4337b8005ae6a6c1022357ec2b129567fce
HiroshiARAKI <araki@hirab.net>
2019-10-27 21:46:42 +0900

    [fix] args of GUILifeGame() was wrong in main.py but fixed.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lifegame import GUILifeGame
 if __name__ == '__main__':
 
     # Create Conway's Game of Life GUI Frame
-    game = GUILifeGame(f_shape=(10, 10), time_step=100)
+    game = GUILifeGame(f_shape=(50, 50), time_step=100)
 
     # Start!!
     game.run(init_rand=True, rate=0.2)
